1ac7b6a1df7c9a7c45c88fcad0e28005c9f4f756
Kimdonghwan <kimdonghwan913@gmail.com>
2024-10-30 00:32:21 +0900

    chore: Vite와 Spring Boot로 초기 프로젝트 구조 설정

---
+++ src/main/java/com/developkim/csprepmate/CsprepmateApplication.java
@@ -0,0 +1,13 @@
+package com.developkim.csprepmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsprepmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsprepmateApplication.class, args);
+    }
+
+}
